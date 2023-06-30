911c67735e207b5773a1bc719ebe371956c4590a
Sidhant Bansal <sidhantbansal1997@gmail.com>
2023-06-30 15:33:21 -0700

    Added logger

--- src/main/java/com/receiptprocessorchallenge/fetchrewards/ReceiptsController.java
+++ src/main/java/com/receiptprocessorchallenge/fetchrewards/ReceiptsController.java
@@ -5,6 +5,8 @@
 package com.receiptprocessorchallenge.fetchrewards;
 
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +18,8 @@ public class ReceiptsController {
 
     private final ReceiptsService receiptsService;
 
+    Logger logger = LoggerFactory.getLogger(ReceiptsController.class);
+
     public ReceiptsController(ReceiptsService receiptsService) {
         this.receiptsService = receiptsService;
     }
@@ -23,13 +27,15 @@ public class ReceiptsController {
     @PostMapping("/process")
     @ResponseBody
     public ResponseEntity<PostResponse> addReceipt(@RequestBody @Valid ReceiptInfo receipt){
-            return ResponseEntity.ok(receiptsService.processReceipts(receipt));
+        logger.info("Processing Receipt");
+        return ResponseEntity.ok(receiptsService.processReceipts(receipt));
     }
 
     @GetMapping("/{id}/points")
     @ResponseBody
     public ResponseEntity<GetResponse> addReceipt(@PathVariable("id") String id){
         try {
+            logger.info("Return points for the receipt");
             return ResponseEntity.ok(receiptsService.getScore(id));
         }
         catch (RuntimeException e){
